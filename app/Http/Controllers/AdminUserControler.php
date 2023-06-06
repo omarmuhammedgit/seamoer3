497aa8ff0230eb3c78f64ed1cec886ff9be6ce71
omar balla <ao22593311@gmail.com>
2023-06-06 07:54:12 +0300

    firsts commit

--- app/Http/Controllers/AdminUserControler.php
+++ app/Http/Controllers/AdminUserControler.php
@@ -11,52 +11,76 @@ use Illuminate\Http\Request;
 class AdminUserControler extends Controller
 {
     public function index(){
+
         $data=Admin::all();
-        return view('admin.index',['data'=>$data]);
+        $com_code=auth()->user()->com_code;
+        if ($com_code!=1) {
+                return redirect()->back()->with('error','عفوا غير مسموح لك باجراء هذا الامر  !!')->withInput();
+        }else{
+            return view('admin.index',['data'=>$data]);
+        }
     }
     public function create(){
+        $com_code=auth()->user()->com_code;
+        if ($com_code!=1) {
+                return redirect()->back()->with('error','عفوا غير مسموح لك باجراء هذا الامر  !!')->withInput();
+        }
         return view('admin.create');
     }
     public function store(AdminRequest $request){
         try {
-            $data=Admin::where('com_code',$request->com_code)->first();
-            if ($data) {
-                return redirect()->back()->with('error','عفوا اسم  المستخدم مسجل من قبل')->withInput();
-            }
-            $data_insert['com_code']=$request->com_code;
-            $data_insert['name']=$request->name;
-            $data_insert['email']=$request->email;
-            $data_insert['username']=$request->username;
-            $data_insert['password']=bcrypt($request->password);
-            $data_insert['active']=$request->active;
-            Admin::create($data_insert);
-            return redirect()->route('user.index')->with('add','تمت اضافة المستخدم بنجاح');
-
+            $com_code=auth()->user()->com_code;
+            if ($com_code!=1) {
+                    return redirect()->back()->with('error','عفوا غير مسموح لك باجراء هذا الامر  !!')->withInput();
+            }else{
+                $data=Admin::where('com_code',$request->com_code)->first();
+                if ($data) {
+                    return redirect()->back()->with('error','عفوا اسم  المستخدم مسجل من قبل')->withInput();
+                }
+                $data_insert['com_code']=$request->com_code;
+                $data_insert['name']=$request->name;
+                $data_insert['email']=$request->email;
+                $data_insert['username']=$request->username;
+                $data_insert['password']=bcrypt($request->password);
+                $data_insert['active']=$request->active;
+                Admin::create($data_insert);
+                return redirect()->route('user.index')->with('add','تمت اضافة المستخدم بنجاح');
+    }
         } catch (\Exception $ex) {
             return redirect()->back()->with('error','عفوا حدث خطا ما'.$ex->getMessage())->withInput();
         }
     }
      public function edit($id){
-        $data=Admin::where('id',$id)->first();
-        return view('admin.edit',['data'=>$data]);
+        $com_code=auth()->user()->com_code;
+        if ($com_code!=1) {
+                return redirect()->back()->with('error','عفوا غير مسموح لك باجراء هذا الامر  !!')->withInput();
+        }else{
+            $data=Admin::where('id',$id)->first();
+            return view('admin.edit',['data'=>$data]);
+
+        }
      }
      public function update(AdminUpdateRequest $request){
         try {
-            $data=Admin::where('com_code',$request->com_code)->where('id','!=',$request->id)->first();
-            if ($data) {
-                return redirect()->back()->with('error','عفوا اسم  المستخدم مسجل من قبل')->withInput();
-            }
-            $data_update['com_code']=$request->com_code;
-            $data_update['name']=$request->name;
-            $data_update['email']=$request->email;
-            if ($request->password !='') {
-                $data_update['password']=bcrypt($request->password);
-            }
-            $data_update['username']=$request->username;
-            $data_update['active']=$request->active;
-            Admin::find($request->id)->update($data_update);
-            return redirect()->route('user.index')->with('add','تمت اضافة المستخدم بنجاح');
-
+            $com_code=auth()->user()->com_code;
+            if ($com_code!=1) {
+                    return redirect()->back()->with('error','عفوا غير مسموح لك باجراء هذا الامر  !!')->withInput();
+            }else{
+                $data=Admin::where('com_code',$request->com_code)->where('id','!=',$request->id)->first();
+                if ($data) {
+                    return redirect()->back()->with('error','عفوا اسم  المستخدم مسجل من قبل')->withInput();
+                }
+                $data_update['com_code']=$request->com_code;
+                $data_update['name']=$request->name;
+                $data_update['email']=$request->email;
+                if ($request->password !='') {
+                    $data_update['password']=bcrypt($request->password);
+                }
+                $data_update['username']=$request->username;
+                $data_update['active']=$request->active;
+                Admin::find($request->id)->update($data_update);
+                return redirect()->route('user.index')->with('add','تمت اضافة المستخدم بنجاح');
+    }
         } catch (\Exception $ex) {
             return redirect()->back()->with('error','عفوا حدث خطا ما'.$ex->getMessage())->withInput();
         }
